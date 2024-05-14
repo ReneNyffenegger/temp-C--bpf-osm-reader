03939b809399beeee0c32d82477646c29a1dc111
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2024-05-14 20:44:49 +0200

    .

---
+++ client.h
@@ -0,0 +1,17 @@
+//
+// Functions that need to be implemented by the client.
+//
+
+#include <time.h>
+
+void osm_node(
+       unsigned long long id,
+       double             lat,
+       double             lon,
+       time_t             ts,
+       unsigned int       version,
+       unsigned long long changeset,
+       int                uid,
+       const char        *user,
+       int                visible
+);
